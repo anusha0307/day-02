b987615a0be191ffea0352101c7d05d91432aecf
anusha <bellamkondaanusha34@gmail.com>
2022-03-03 21:49:55 +0530

    type of a value

---
+++ script.js
@@ -0,0 +1,20 @@
+
+// 4.Execute the below code and write your description in txt file
+let x=1
+b=1.1
+c=1.1
+d=[]
+e=true
+f=null
+g=undefined
+o={}
+i=NaN
+console.log(typeof(x))
+console.log(typeof(b))
+console.log(typeof(c))
+console.log(typeof(d))
+console.log(typeof(e))
+console.log(typeof(f))
+console.log(typeof(g))
+console.log(typeof(o))
+console.log(typeof(i))
